be0b2e434c283fbb3090a91099dfceb5ebe92d34
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 03:08:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if (array.length==1)
     return array[0]
   elsif(array.length==2)
-    return "#{a[0]} and #{a[1]}"
+    return "#{array[0]} and #{array[1]}"
   else
     a2=array
     last=a2.pop
